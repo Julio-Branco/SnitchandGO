a336ed0cd4d58feef01a2a9b6ab199a9e26b2c12
hugodrevet <hugo.drevet@hotmail.fr>
2024-09-24 15:52:56 +0200

    fix

--- snitchgo/src/components/Historique.js
+++ snitchgo/src/components/Historique.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import Report from './Report'
+import React from "react";
+import Report from "./Report";
 
-export default function Historique({props}){
-    return (
-        <>
-            <h1>Dénonciations</h1>
-            <div className='hr'/>
-            <Report name="John" firstname="Doe" date="2024-24-09 11:10:45" type="R" nbVote={3} />
-        </>
-    )
-}
+export const Historique = ({ props }) => {
+  return (
+    <>
+      <h1>Dénonciations</h1>
+      <div className="hr" />
+      <Report
+        name="John"
+        firstname="Doe"
+        date="2024-24-09 11:10:45"
+        type="R"
+        nbVote={3}
+      />
+    </>
+  );
+};
