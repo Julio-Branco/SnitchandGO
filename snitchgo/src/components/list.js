442b242e4b1a3ce10b91779273e7b519af661cd5
Julio-Branco <j.costabranco@ecoles-epsi.net>
2024-09-24 13:59:19 +0200

    Change saves

--- snitchgo/src/components/list.js
+++ snitchgo/src/components/list.js
@@ -11,9 +11,9 @@ export default function list(){
         return (
             <div className="flex">
                 <div className="item">
+                    <img src={data.personnes[0].pp} width={80} height={80} className="rounded-circle" alt="" />
+
                     <div className="info">
-                        <img src={data.personnes[0].pp.url} />
-                        <span></span>
                         <h3 className="nom text-dark">{data.personnes[0].prenom} {data.personnes[0].nom}</h3>
                         <span>{data.personnes[0].score}</span>
                     </div>
