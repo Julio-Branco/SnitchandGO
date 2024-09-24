1aa2a91c61f277c261379c6f97dd7906dcef8254
Julio-Branco <j.costabranco@ecoles-epsi.net>
2024-09-24 16:18:00 +0200

    fix

--- snitchgo/src/pages/Home.js
+++ snitchgo/src/pages/Home.js
@@ -13,6 +13,7 @@ export const Home = () => {
                     </div>
                     <div className="col-6 my-4 h-75" style={{ background: "#EAEAEB", "border-radius": '15px', 'box-shadow': 'rgba(0, 0, 0, 0.35) 0px 5px 15px' }}>
                     </div>
+                    
                 </div>
             </div >
 
