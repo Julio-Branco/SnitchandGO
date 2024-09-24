58802529413dc46f5d92d1bc21421b638c9f699a
Julio-Branco <j.costabranco@ecoles-epsi.net>
2024-09-24 11:26:54 +0200

    fix before pull

--- snitchgo/src/components/Ranking.js
+++ snitchgo/src/components/Ranking.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import List from './list'
 
 export default function Ranking(){
     return (
         <div className="ranking">
             <h1>Leaderboard</h1>
+        <List></List>
         </div>
+
     )
 }
