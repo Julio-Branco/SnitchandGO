ce50d3aa0f7fd027f4490f38de864ed379c8f7e0
Vincent <vincentbullion@icloud.com>
2024-09-24 10:03:07 +0200

    Modified dashboard

--- snitchgo/src/components/Dashboard.js
+++ snitchgo/src/components/Dashboard.js
@@ -3,7 +3,7 @@ import { Navbar } from "./Navbar"
 export const Dashboard = () => {
     return (
         <>
-            <div className="flex max-w justify-content-center align-items-center">
+            <div className="flex justify-content-center align-items-center">
                 <Navbar/>
             </div>
         </>
