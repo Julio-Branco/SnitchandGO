4b2eba7f1d4ec370a2e7376d1fec658af21940e7
Julio-Branco <j.costabranco@ecoles-epsi.net>
2024-09-24 10:03:53 +0200

    Commit Ranking

--- snitchgo/src/App.js
+++ snitchgo/src/App.js
@@ -2,11 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Dashboard } from "./components/Dashboard";
 import Profile from "./pages/Profile";
+import Ranking from "./components/Ranking"
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      
+      <Ranking></Ranking>
     </div>
   );
 }
