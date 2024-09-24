6f183d65e83a390eaa05728e2508eefb5f857f5f
Vincent <vincentbullion@icloud.com>
2024-09-24 23:18:58 +0200

    Finished modifying json using api

---
+++ snitchgo/src/api/reports.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const getReports = async () => {
+    const response = await axios.get('http://localhost:5001/reports');
+    return response.data;
+};
+
+export const voteReport = async (id) => {
+    await axios.post('http://localhost:5001/reports/vote', { id });
+};
+
+export const dismissReport = async (id) => {
+    await axios.post('http://localhost:5001/reports/dismiss', { id });
+};
--- snitchgo/src/components/Historique.js
+++ snitchgo/src/components/Historique.js
@@ -1,19 +1,39 @@
-import React from 'react'
-import Report from './Report'
-import data from '../Data/Data.json'
+import React, { useState, useEffect } from 'react';
+import { getReports, voteReport, dismissReport } from '../api/reports';
+import Report from './Report';
+import data from '../Data/Data.json';
 
 export default function Historique() {
+    const [reports, setReports] = useState([]);
+
+    useEffect(() => {
+        const fetchReports = async () => {
+            const reportsData = await getReports();
+            setReports(reportsData);
+        };
+        fetchReports();
+    }, []);
+
+    const handleDismiss = async (id) => {
+        await dismissReport(id);
+        const updatedReports = await getReports();
+        setReports(updatedReports); 
+    };
+
+    const handleVote = async (id) => {
+        await voteReport(id);
+        const updatedReports = await getReports();
+        setReports(updatedReports); 
+    };
+
     return (
-        <div style={{ 
-            overflowY: 'scroll',
-            height: '500px',
-            padding: '10px'
-        }}>
+        <div style={{ overflowY: 'scroll', height: '500px', padding: '10px' }}>
             <h1>Dénonciations</h1>
             <hr className='w-100 d-flex justify-self-center' style={{ color: "#F43535" }} />
-            {data.reports.map((report, index) => (
+            {reports.map((report, index) => (
                 <div key={index} style={{ marginBottom: '20px' }}>
                     <Report
+                        id={report.id}
                         name={report.name}
                         firstname={report.firstname}
                         date={report.date}
@@ -21,10 +41,11 @@ export default function Historique() {
                         nbVote={report.nbVote}
                         cours={report.cours}
                         display={report.display}
+                        onDismiss={handleDismiss} // Pass the dismiss handler
+                        onVote={handleVote} // Pass the vote handler
                     />
                 </div>
             ))}
         </div>
-    )
+    );
 }
-    
